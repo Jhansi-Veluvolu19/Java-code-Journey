2fe9e5b5e4fb2f6144241c6a40957edbb59b9f34
jhansi-veluvolu19 <jhansiv2025@gmail.com>
2025-10-24 11:14:34 -0400

    added Num files

---
+++ Num.java
@@ -0,0 +1,9 @@
+public class Num {
+    public static void main (String a[])
+    {
+        int num1 = 9;
+        int num2 = 5+3;
+        int result = num1+num2;
+        System.out.print(result);
+    }
+}
