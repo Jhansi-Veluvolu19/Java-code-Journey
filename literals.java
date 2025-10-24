d8795cf31a3853d7e094e7a3e6fb88992d931772
jhansi-veluvolu19 <jhansiv2025@gmail.com>
2025-10-24 12:53:20 -0400

    added literals

---
+++ literals.java
@@ -0,0 +1,16 @@
+ class literals {
+    public static void main (String a[])
+    {
+        // literals
+
+        int num1 = 0b101;
+        int num2 = 0x7E;
+        int num3 = 10_00_00_000;
+        double num4 = 12e10;
+        boolean b = true;
+        char c = 'a';
+        c++;
+        
+        System.out.println(c);
+    }
+}
