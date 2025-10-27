bff6d481cf943b8b6ad241f3eba8181c9fd3356f
jhansi-veluvolu19 <jhansiv2025@gmail.com>
2025-10-27 09:56:26 -0400

    added loop  files

---
+++ loop.java
@@ -0,0 +1,22 @@
+public class loop {
+    public static void main (String []a)
+    {
+        int i = 1;
+
+        while(i<=4)
+
+        {
+            System.out.println ("Hi " + i);
+            int j = 1;
+            while(j<=3){
+                System.out.println ("Hello " + j);
+                j++;
+        }
+
+            i++;
+    }
+            System.out.println ("Bye " + i);
+    }
+                         
+    }
+
